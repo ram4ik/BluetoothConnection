5a817e589de991216802459a7100203bac6fd4d8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-16 14:52:23 +0300

    init

--- BluetoothConnection/ViewController.swift
+++ BluetoothConnection/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 
+    var manager: CBCentralManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
-
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        
+        print("NAME: \(String(describing: peripheral.name))")
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        var msg = ""
+        switch (central.state) {
+        case .poweredOff:
+            msg = "BT off"
+        case .poweredOn:
+            msg = "BT on"
+            manager.scanForPeripherals(withServices: nil, options: nil)
+        case .unsupported:
+            msg = "BT unsupported"
+        default:
+            break
+        }
+        print("STATE: \(msg)")
+    }
 
 }
 
